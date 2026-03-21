606a7f238920642efbf8b5296036f25c8b99e55d
Deluan Quintão <deluan@deluan.com>
2026-03-20 20:34:11 -0400

    feat: use Cover Art Archive for albums with MusicBrainz IDs (#27)

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ const (
 	usersKey        = "users"
 	activityNameKey = "activityname"
 	spotifyLinksKey = "spotifylinks"
+	caaEnabledKey   = "caaenabled"
+	uguuEnabledKey  = "uguuenabled"
 )
 
 const (
@@ -193,7 +195,7 @@ func (p *discordPlugin) NowPlaying(input scrobbler.NowPlayingRequest) error {
 			End:   endTime,
 		},
 		Assets: activityAssets{
-			LargeImage: getImageURL(input.Username, input.Track.ID),
+			LargeImage: getImageURL(input.Username, input.Track),
 			LargeText:  input.Track.Album,
 			LargeURL:   spotifyURL,
 			SmallImage: navidromeLogoURL,
